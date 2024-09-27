a81138ad08f7d15c1e028c11a25bd70339fd7523
Luana Tajes <lmluana27@gmail.com>
2024-09-27 00:52:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "A",
+    "J",
+    "Q",
+    "K"
+  ];
+  let cardPintas = ["♦", "♥", "♠", "♣"];
+  function elegirAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  const valorAleatorio = elegirAleatorio(cardValues);
+  document.querySelector(".value").innerHTML = valorAleatorio;
+  const pintaAleatoria = elegirAleatorio(cardPintas);
 };
